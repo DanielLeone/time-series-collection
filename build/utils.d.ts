8fec35da7ed206f6ce1a0c5076eec68bf9f0383e
Daniel Leone <mail.dleone@gmail.com>
2018-04-08 19:12:25 +0800

    removed ignore files

--- build/utils.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function isNumber(val: number): boolean;
-export declare function isValidTimestamp(timestamp: number): boolean;
-export declare function isNumberOrInfinity(val: number): boolean;
-export declare function isValidTimeRange(fromTimestamp: number, toTimestamp: number): boolean;
-export declare function binarySearch(array: Array<number>, target: number): number;
